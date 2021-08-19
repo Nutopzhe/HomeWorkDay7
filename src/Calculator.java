930a45e6b21d3f9d18536141183d491d9a833626
Nikolay Rybakov <mr.Nikolay.Rybakov@yandex.ru>
2021-08-19 13:18:06 +0300

    Added basic logic to the program and refactor class Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,17 +1,17 @@
 public class Calculator {
-    public double addition(double d1, double d2) {
+    public double addition(String d1, String d2) {
         return 0;
     }
 
-    public double subtract(double d1, double d2) {
+    public double subtract(String d1, String d2) {
         return 0;
     }
 
-    public double multiply(double d1, double d2) {
+    public double multiply(String d1, String d2) {
         return 0;
     }
 
-    public double divide(double d1, double d2) {
+    public double divide(String d1, String d2) {
         return 0;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+        String inputString;
 
+        while (true) {
+            System.out.println("Введите первый аргумент, операцию, второй аргумент через пробел!");
+
+            inputString = scanner.nextLine();
+            String[] arrayInputString = inputString.split("\\s");
+
+            if (arrayInputString.length != 3) {
+                System.out.println("Неправильный формат ввода!");
+                continue;
+            }
+
+            switch (arrayInputString[1]) {
+                case "плюс":
+                    System.out.println(calculator.addition(arrayInputString[0],
+                            arrayInputString[2]));
+                    break;
+                case "минус":
+                    System.out.println(calculator.subtract(arrayInputString[0],
+                            arrayInputString[2]));
+                    break;
+                case "умножить":
+                    System.out.println(calculator.multiply(arrayInputString[0],
+                            arrayInputString[2]));
+                    break;
+                case "разделить":
+                    System.out.println(calculator.divide(arrayInputString[0],
+                            arrayInputString[2]));
+                    break;
+            }
+        }
     }
 }
