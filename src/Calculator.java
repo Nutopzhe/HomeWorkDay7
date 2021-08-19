321aeb061e2147470492e1174914ec3e34c5a6a3
Nikolay Rybakov <mr.Nikolay.Rybakov@yandex.ru>
2021-08-19 13:11:57 +0300

    Added stubs to class Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,17 @@
 public class Calculator {
+    public double addition(double d1, double d2) {
+        return 0;
+    }
+
+    public double subtract(double d1, double d2) {
+        return 0;
+    }
+
+    public double multiply(double d1, double d2) {
+        return 0;
+    }
+
+    public double divide(double d1, double d2) {
+        return 0;
+    }
 }
