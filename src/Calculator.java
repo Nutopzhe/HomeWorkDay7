53238eea40fadf0e3350cac0ed4a9ccb7b83fc69
Nikolay Rybakov <mr.Nikolay.Rybakov@yandex.ru>
2021-08-19 13:41:59 +0300

    Added implementation to methods of Calculator class. Added try-catch blocks in Main class.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,17 +1,29 @@
 public class Calculator {
-    public double addition(String d1, String d2) {
-        return 0;
+    public double addition(String d1, String d2) throws NumberFormatException {
+        Double firstDigit = Double.parseDouble(d1);
+        Double secondDigit = Double.parseDouble(d2);
+
+        return firstDigit + secondDigit;
     }
 
-    public double subtract(String d1, String d2) {
-        return 0;
+    public double subtract(String d1, String d2) throws NumberFormatException {
+        Double firstDigit = Double.parseDouble(d1);
+        Double secondDigit = Double.parseDouble(d2);
+
+        return firstDigit - secondDigit;
     }
 
-    public double multiply(String d1, String d2) {
-        return 0;
+    public double multiply(String d1, String d2) throws NumberFormatException {
+        Double firstDigit = Double.parseDouble(d1);
+        Double secondDigit = Double.parseDouble(d2);
+
+        return firstDigit * secondDigit;
     }
 
-    public double divide(String d1, String d2) {
-        return 0;
+    public double divide(String d1, String d2) throws NumberFormatException {
+        Double firstDigit = Double.parseDouble(d1);
+        Double secondDigit = Double.parseDouble(d2);
+
+        return firstDigit / secondDigit;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,33 +7,55 @@ public class Main {
         String inputString;
 
         while (true) {
-            System.out.println("Введите первый аргумент, операцию, второй аргумент через пробел!");
+            System.out.println("Введите первую цифру, операцию, вторую цифру через пробел!");
 
             inputString = scanner.nextLine();
             String[] arrayInputString = inputString.split("\\s");
 
             if (arrayInputString.length != 3) {
-                System.out.println("Неправильный формат ввода!");
+                System.err.println("Неправильный формат ввода!");
                 continue;
             }
 
             switch (arrayInputString[1]) {
                 case "плюс":
-                    System.out.println(calculator.addition(arrayInputString[0],
-                            arrayInputString[2]));
+                    try {
+                        System.out.println(calculator.addition(arrayInputString[0],
+                                arrayInputString[2]));
+                    } catch (NumberFormatException e) {
+                        System.err.println("Неверный формат цифр!");
+                        e.printStackTrace();
+                    }
                     break;
                 case "минус":
-                    System.out.println(calculator.subtract(arrayInputString[0],
-                            arrayInputString[2]));
+                    try {
+                        System.out.println(calculator.subtract(arrayInputString[0],
+                                arrayInputString[2]));
+                    } catch (NumberFormatException e) {
+                        System.err.println("Неверный формат цифр!");
+                        e.printStackTrace();
+                    }
                     break;
                 case "умножить":
-                    System.out.println(calculator.multiply(arrayInputString[0],
-                            arrayInputString[2]));
+                    try {
+                        System.out.println(calculator.multiply(arrayInputString[0],
+                                arrayInputString[2]));
+                    } catch (NumberFormatException e) {
+                        System.err.println("Неверный формат цифр!");
+                        e.printStackTrace();
+                    }
                     break;
                 case "разделить":
-                    System.out.println(calculator.divide(arrayInputString[0],
-                            arrayInputString[2]));
+                    try {
+                        System.out.println(calculator.divide(arrayInputString[0],
+                                arrayInputString[2]));
+                    } catch (NumberFormatException e) {
+                        System.err.println("Неверный формат цифр!");
+                        e.printStackTrace();
+                    }
                     break;
+                default:
+                    System.err.println("Неправильная операция! Для подсказки введите: help");
             }
         }
     }
